5e312bd9ce4d84880b4b1382fd2c81ab29ab2541
ololome <6f6c6f6c6f6d65@gmail.com>
2024-05-08 13:21:42 +0200

    test NODE_ENV

--- website/vite.config.mjs
+++ website/vite.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 
+console.log(process.env.NODE_ENV);
+console.log(process.env.NODE_ENV === 'production');
+
 export default defineConfig({
   base: "/ololo.me/",
 });
